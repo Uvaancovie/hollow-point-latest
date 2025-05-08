11ad4038b88edff98bb4cfc827f9ceda160a77af
Uvaancovie <uvaancovie@gmail.com>
2025-05-08 20:42:32 +0200

    new changes of the about

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -5,9 +5,17 @@ import DurbanInfo from '../sections/DurbanInfo';
 const AboutPage = () => {
   return (
     <div className="pt-20">
-      <div className="bg-black py-16">
-        <h1 className="text-white text-4xl font-bold text-center">About Us</h1>
-      </div>
+    <div
+        className="relative bg-cover bg-center py-16"
+        style={{
+            backgroundImage: `url('https://images.pexels.com/photos/4646677/pexels-photo-4646677.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')`,
+        }}
+    >
+        <div className="absolute inset-0 bg-black opacity-50" />
+        <h1 className="relative text-white text-4xl font-bold text-center">
+            About Us
+        </h1>
+    </div>
       <About />
       <DurbanInfo />
     </div>
