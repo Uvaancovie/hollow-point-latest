cc0f50f6387c2b6a03dce6d399f56786b07ef5a7
Uvaancovie <uvaancovie@gmail.com>
2025-05-08 20:45:45 +0200

    the products header changed

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -4,9 +4,18 @@ import Products from '../sections/Products';
 const ProductsPage = () => {
   return (
     <div className="pt-20">
-      <div className="bg-black py-16">
-        <h1 className="text-white text-4xl font-bold text-center">Our Products</h1>
-      </div>
+    <div
+      className="relative bg-cover bg-center py-16"
+      style={{
+        backgroundImage:
+        "url('https://images.pexels.com/photos/430208/pexels-photo-430208.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')",
+      }}
+    >
+      <div className="absolute inset-0 bg-black opacity-50"></div>
+      <h1 className="relative text-white text-4xl font-bold text-center">
+        Our Products
+      </h1>
+    </div>
       <Products />
     </div>
   );
